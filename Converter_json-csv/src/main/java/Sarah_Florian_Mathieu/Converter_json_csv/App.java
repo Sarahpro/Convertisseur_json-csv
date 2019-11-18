dc362c4b42de2e27af8b32b030ebda0034a07caf
Mathieu <mathieu.de-jesus@ens.uvsq.fr>
2019-11-18 20:35:49 +0100

    Intégration maven et l'ensemble des plugins + jar executable

---
+++ Converter_json-csv/src/main/java/Sarah_Florian_Mathieu/Converter_json_csv/App.java
@@ -0,0 +1,15 @@
+package Sarah_Florian_Mathieu.Converter_json_csv;
+
+/**
+ * Hello world!
+ *
+ */
+public enum App 
+{
+	APPPLICATION;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
