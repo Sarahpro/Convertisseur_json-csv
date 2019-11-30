8f7c3d037d36c3d44cffa063db6663f708520656
= <ledeme.florian@gmail.com>
2019-11-30 22:44:32 +0100

    Initialisation jsonManager

--- Converter_json-csv/src/main/java/Sarah_Florian_Mathieu/Converter_json_csv/jsonManager.java
+++ Converter_json-csv/src/main/java/Sarah_Florian_Mathieu/Converter_json_csv/jsonManager.java
@@ -1,5 +1,25 @@
 package Sarah_Florian_Mathieu.Converter_json_csv;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class jsonManager {
-	
+    private int Acc_number; //Nombre de crochets lus (utile pour vérification structure fichier)
+    private int Hk_number; //Nombre d'accolades lues
+    private final char openObjectSeparator = '{';
+    private final char closeObjectSeparator = '}';
+    private final char openObjectElementsSeparator = '[';
+    private final char closeObjectElementsSeparator = ']';
+
+    /** class constructor
+     *
+     */
+    public jsonManager(){
+        this.Acc_number = 0;
+        this.Hk_number = 0;
+    }
+
+
 }
